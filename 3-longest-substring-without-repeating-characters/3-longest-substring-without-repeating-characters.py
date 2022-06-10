4c85758b9945e0c02261e5c90af1e08d71d2b4d5
myselfme1 <77435681+myselfme1@users.noreply.github.com>
2022-06-11 00:19:15 +0530

    Time: 2751 ms (5.00%), Space: 14.1 MB (49.19%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,21 +1,17 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        L={'kk'}
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        L=[]
         c=0        
         for i in range(len(s)):
             for ch in range(i,len(s)):
-                if s[ch] not in L:
-                    #print(s[ch])
-                    L.add(s[ch])
+                if s[ch] not in L: 
+                    L.append(s[ch])
                 else:
                     if len(L)>c:
-                        #print(L)
                         c=len(L)
-                    L={'kk'}
+                    L=[]
                     break             
         if len(L)<c:
-            return c-1
+            return c
         else:
-            return len(L)-1
-                
-        
+            return len(L)
