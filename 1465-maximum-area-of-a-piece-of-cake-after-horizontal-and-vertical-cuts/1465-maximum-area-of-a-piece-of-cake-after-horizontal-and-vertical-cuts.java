8669e53116ad42faac1d83dd9cd16b37e90758ca
myselfme1 <77435681+myselfme1@users.noreply.github.com>
2022-07-03 02:01:51 +0530

    Time: 16 ms (98.49%), Space: 52 MB (85.63%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts)
+    {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        long l1,l2;
+        if(horizontalCuts.length==0)
+            l1=h;
+        else if(horizontalCuts.length==1)
+            l1=horizontalCuts[0]-0>h-horizontalCuts[0]?horizontalCuts[0]-0:
+        h-horizontalCuts[horizontalCuts.length-1];
+        else
+        {
+            l1=horizontalCuts[0]-0;
+            for(int i=1;i<horizontalCuts.length;i++)
+            {
+                if(horizontalCuts[i]-horizontalCuts[i-1]>l1)
+                    l1=horizontalCuts[i]-horizontalCuts[i-1];
+            }
+            if(h-horizontalCuts[horizontalCuts.length-1]>l1)
+                l1=h-horizontalCuts[horizontalCuts.length-1];
+            
+        }
+        if(verticalCuts.length==0)
+            l2=w;
+        else if(verticalCuts.length==1)
+            l2=verticalCuts[0]-0>w-verticalCuts[0]?verticalCuts[0]-0:
+        w-verticalCuts[verticalCuts.length-1];
+        else
+        {
+            l2=verticalCuts[0]-0;
+            for(int i=1;i<verticalCuts.length;i++)
+            {
+                if(verticalCuts[i]-verticalCuts[i-1]>l2)
+                    l2=verticalCuts[i]-verticalCuts[i-1];
+            }
+            if(w-verticalCuts[verticalCuts.length-1]>l2)
+                l2=w-verticalCuts[verticalCuts.length-1];
+            
+        }
+        return (int)(l1*l2%1000000007);
+    }
+}
